db7319d73a835ca86a8619683867941d963d72ff
TheoPadilha <168146283+TheoPadilha@users.noreply.github.com>
2025-04-13 19:27:35 -0300

    Revert "commit"
    This reverts commit 7afa5c176f342617f15982392a34c69a0c079328.

--- vera-beleza/src/components/Hero.jsx
+++ vera-beleza/src/components/Hero.jsx
@@ -1,11 +1,10 @@
 import "./hero.css";
 import "./Navbar.css";
-import manicureImg from "../assets/upscalemedia-transformed (2) (1).png";
+// import manicureImg from "";
 
 const Home = () => {
   return (
     <div className="hero-container">
-      
       <div className="blob1">
         <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
           <path
@@ -17,42 +16,37 @@ const Home = () => {
       </div>
 
       {/* NAVBAR */}
-      <div className="nav_rei">
-        <header className="navbar">
-          <div className="navbar-container">
-            <div className="logo">Vera Alice</div>
-            <div className="navLink_colorido">
-              <ul className="nav-links">
-                <li>
-                  <a href="#">Início</a>
-                </li>
-                <li>
-                  <a href="#">Serviços</a>
-                </li>
-                <li>
-                  <a href="#">Sobre</a>
-                </li>
-                <li>
-                  <a href="#">Contato</a>
-                </li>
-                <div className="blob-header">
-                  <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                      fill="#F4B395"
-                      d="M45.8,-38.1C58.2,-33.4,66.2,-16.7,61.8,-4.4C57.5,8,40.7,16,28.3,27C16,38,8,52.1,-3.4,55.5C-14.8,58.9,-29.6,51.6,-42.7,40.6C-55.9,29.6,-67.4,14.8,-64.5,2.9C-61.6,-9,-44.3,-17.9,-31.1,-22.6C-17.9,-27.2,-9,-27.5,3.9,-31.4C16.7,-35.3,33.4,-42.7,45.8,-38.1Z"
-                      transform="translate(100 100)"
-                    />
-                  </svg>
-                </div>
-              </ul>
+      <header className="navbar">
+        <div className="navbar-container">
+          <div className="logo">Vera Alice</div>
+          <ul className="nav-links">
+            <li>
+              <a href="#">Início</a>
+            </li>
+            <li>
+              <a href="#">Serviços</a>
+            </li>
+            <li>
+              <a href="#">Sobre</a>
+            </li>
+            <li>
+              <a href="#">Contato</a>
+            </li>
+            <div className="blob-header">
+              <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  fill="#F4B395"
+                  d="M45.8,-38.1C58.2,-33.4,66.2,-16.7,61.8,-4.4C57.5,8,40.7,16,28.3,27C16,38,8,52.1,-3.4,55.5C-14.8,58.9,-29.6,51.6,-42.7,40.6C-55.9,29.6,-67.4,14.8,-64.5,2.9C-61.6,-9,-44.3,-17.9,-31.1,-22.6C-17.9,-27.2,-9,-27.5,3.9,-31.4C16.7,-35.3,33.4,-42.7,45.8,-38.1Z"
+                  transform="translate(100 100)"
+                />
+              </svg>
             </div>
-          </div>
-        </header>
-      </div>
+          </ul>
+        </div>
+      </header>
 
       {/* CONTEÚDO PRINCIPAL */}
       <main className="hero-main">
-      <img className="img-mulheres" src={manicureImg} alt="" />
         <div className="hero-text">
           <h1>
             Cuidando de você <br />
