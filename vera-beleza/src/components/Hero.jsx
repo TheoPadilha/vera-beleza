045c90c042ef9b95d03ff97484d3d2c831b8fcf3
TheoPadilha <168146283+TheoPadilha@users.noreply.github.com>
2025-04-14 11:03:03 -0300

    commit

--- vera-beleza/src/components/Hero.jsx
+++ vera-beleza/src/components/Hero.jsx
@@ -1,21 +1,10 @@
 import "./hero.css";
 import "./Navbar.css";
 import manicureImg from "../assets/upscalemedia-transformed (2) (1).png";
-
+import { Blob, Blob2 } from "./Blob.jsx";
 const Home = () => {
   return (
     <div className="hero-container">
-      
-      <div className="blob1">
-        <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-          <path
-            fill="#C69C6C"
-            d="M55.2,-54.7C66.7,-43.8,67.8,-21.9,64.7,-3.1C61.6,15.7,54.4,31.4,42.9,46.6C31.4,61.8,15.7,76.4,-0.5,77C-16.8,77.5,-33.6,64,-48.1,48.8C-62.7,33.6,-74.9,16.8,-74.3,0.6C-73.7,-15.6,-60.2,-31.2,-45.7,-42.1C-31.2,-53.1,-15.6,-59.4,3.2,-62.5C21.9,-65.7,43.8,-65.7,55.2,-54.7Z"
-            transform="translate(100 100)"
-          />
-        </svg>
-      </div>
-
       {/* NAVBAR */}
       <div className="nav_rei">
         <header className="navbar">
@@ -52,26 +41,30 @@ const Home = () => {
 
       {/* CONTEÚDO PRINCIPAL */}
       <main className="hero-main">
-      <img className="img-mulheres" src={manicureImg} alt="" />
+        <Blob2 />
+        <Blob />
+
+        <div className="img-mulheres">
+          <img src={manicureImg} alt="" />
+        </div>
         <div className="hero-text">
           <h1>
             Cuidando de você <br />
             por inteiro
           </h1>
+          <h2>Beleza que começa no cuidado com você</h2>
           <p>
             Manicure, Pedicure, Depilação, Terapias, <br />
             Massagens, Reiki, Reflexologia e muito mais.
           </p>
           <div className="botaoAgendarHrr">
-            <button>
-              <a
-                className="agendehorario"
-                href="https://wa.me/5547984731947?text=Olá%2C%20gostaria%20de%20agendar%20um%20horário%20com%20a%20Vera%20Alice!"
-                target="blank"
-              >
-                Agende um horário
-              </a>
-            </button>
+            <a
+              className="agendehorario"
+              href="https://wa.me/5547984731947?text=Olá%2C%20gostaria%20de%20agendar%20um%20horário%20com%20a%20Vera%20Alice!"
+              target="blank"
+            >
+              Agende um horário
+            </a>
           </div>
         </div>
 
